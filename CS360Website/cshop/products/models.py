a1d51d72e9a1193d3e53beaed15a3ffa4e615b3b
OscarTheGrouch2 <oscarmichua6@gmail.com>
2022-04-13 19:52:46 -0700

    Added Classes

--- CS360Website/cshop/products/models.py
+++ CS360Website/cshop/products/models.py
@@ -1,3 +1,4 @@
+from telnetlib import STATUS
 from django.db import models
 
 # Create your models here.
@@ -68,6 +69,57 @@ class Customer(models.Model):
     Email = models.EmailField(max_length=256, null = False)
     Address = models.ManyToManyField(Address)
     Phone = models.IntegerField(max_length=10, null = True)
+    def __str__(self):
+        return self.field_name
+    def get_absolute_url(self):
+        return reverse('model-detail-view', args=[str(self.id)])
+
+class Orders(models.Model):
+    Status = models.CharField(max_length=99, null = False)
+    OrderNumber = models.IntegerField(max_length=50, null = False)
+    TrackingNumber = models.IntegerField(max_length=50, null = False)
+    ShippingAddress = models.ManyToManyField(Address)        
+    def __str__(self):
+        return self.field_name
+    def get_absolute_url(self):
+        return reverse('model-detail-view', args=[str(self.id)])
+
+class PaymentMethod(models.Model):
+    CardNumber = models.IntegerField(max_length=16, null = False)
+    CardCompany = models.CharField(max_length=99, null = False)
+    NameOnCard = models.CharField(max_length=99, null = False)
+    Expiration = models.IntegerField(max_length=6, null = False)
+    CVV = models.IntegerField(max_length=3, null = False)
+    CardType = models.CharField(max_length=99, null = False)
+    BillingAddress = models.ManyToManyField(Address)
+    def __str__(self):
+        return self.field_name
+    def get_absolute_url(self):
+        return reverse('model-detail-view', args=[str(self.id)])
+
+class Product(models.Model):
+    ProductName = models.CharField(max_length=99, null = False)
+    #Inventory as a integer for count?
+    Inventory = models.IntegerField(max_length=99, null = False)
+    # file will be uploaded to MEDIA_ROOT / uploads                (either FileField or ImageField should be used)
+    Picture = models.ImageField(upload_to = 'uploads/') 
+    Type = models.CharField(max_length=99, null = False)
+    Price = CVV = models.IntegerField(max_length=10, null = False)
+    Rating  = CVV = models.IntegerField(max_length=5, null = False) # 1-5 rating?
+    ProductID = CVV = models.IntegerField(max_length=99, null = False)
+    ShippingCost =  CVV = models.IntegerField(max_length=10, null = False)
+    def __str__(self):
+        return self.field_name
+    def get_absolute_url(self):
+        return reverse('model-detail-view', args=[str(self.id)])
+
+class Company(models.Model):
+    CompanyName = models.CharField(max_length=99, null = False)
+    SupportEmail = models.EmailField(max_length=256, null = False)
+    SupportPhone = models.IntegerField(max_length=10, null = True)
+    CompanyAddress = models.ManyToManyField(Address)
+    CompanyID =  models.IntegerField(max_length=10, null = False)
+    AboutUs = models.CharField(max_length=512, null = False)
     def __str__(self):
         return self.field_name
     def get_absolute_url(self):
